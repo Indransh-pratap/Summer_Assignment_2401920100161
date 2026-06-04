529b4363fe6538a06b97adc2e3af1dd41838f4ee
khyatiydv21 <indranshpratap@gmail.com>
2026-06-04 12:43:57 +0530

    H

---
+++ Week_1/Day_3/q1.move_zeros.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+
+int main(){
+    vector<int>arr ={2,3,4,5,0,8,0,9,0,8,0};
+    Solution obj;
+    cout<<movesZeroes
+}
